d9541083782768c3c2e3ec9721b6965dead4879d
Angel Nuñez Figueroa <158320406+AngelNz21@users.noreply.github.com>
2026-02-14 17:14:12 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  images: {
+    unoptimized: true
+  },
+
+  basePath: "/carta-para-mor",
+  assetPrefix: "/carta-para-mor/",
+
   typescript: {
     ignoreBuildErrors: true,
   }
